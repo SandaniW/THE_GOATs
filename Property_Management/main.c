f4fabac6266595e6d0b8359ff6c3b49b15685d49
sandaniW <sandaniwickramanayake04@gmail.com>
2024-08-29 19:53:14 +0530

    made the c file for the project

---
+++ Property_Management/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+//AS2023952
+//add up your indexes
+
+//you all need to choose 1 function and work on it :)
+/*
+1. Contractor Management:
+2. Property Management:
+3. Property Status Updates:
+4. Price Calculation:
+5. Reports:
+6. User Interface:
+- Error Handling
+*/
+
+int main()
+{
+
+    return 0;
+}
